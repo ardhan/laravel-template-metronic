853b8b4b0aa0d11493a9ae70c355e61b8d8b5856
Ardhan <ardhan.matematika@gmail.com>
2020-08-07 07:29:24 +0700

    Finishing sweetalert

--- src/MetronicSweetAlert.php
+++ src/MetronicSweetAlert.php
@@ -1,77 +1,135 @@
 <?php
-/*$('#kt_sweetalert_demo_9').click(function(e) {
-            swal.fire({
-                title: 'Are you sure?',
-                text: "You won't be able to revert this!",
-                type: 'warning',
-                showCancelButton: true,
-                confirmButtonText: 'Yes, delete it!',
-                cancelButtonText: 'No, cancel!',
-                reverseButtons: true
-            }).then(function(result){
-                if (result.value) {
-                    swal.fire(
-                        'Deleted!',
-                        'Your file has been deleted.',
-                        'success'
-                    )
-                    // result.dismiss can be 'cancel', 'overlay',
-                    // 'close', and 'timer'
-                } else if (result.dismiss === 'cancel') {
-                    swal.fire(
-                        'Cancelled',
-                        'Your imaginary file is safe :)',
-                        'error'
-                    )
-                }
-            });
-        });*/
-        namespace Ardhan\LaravelTemplateMetronic;
-        use Ardhan\LaravelSimpleHtml\Page;
-        use Ardhan\LaravelSimpleHtml\Facades\Element as El;
-        use Ardhan\LaravelTemplateMetronic\Facades\Metronic;
+namespace Ardhan\LaravelTemplateMetronic;
 
 class MetronicSweetAlert
 {
+    /**
+     * nama kelas yang menjadi selector
+     * @var string
+     */
     private $selector = '';
-    private $text = '';
+
+    /**
+     * judul pada modal yang muncul
+     * @var string
+     */
     private $title = '';
+
+    /**
+     * isi notifikasi
+     * @var string
+     */
+    private $text = '';
+
+    /**
+     * url yang menjadi tujuan setelah user ok
+     * @var string
+     */
+    private $url = '';
+
+    /**
+     * jenis notifikasi
+     * @var string
+     */
     private $type = 'warning';
+
+    /**
+     * munculkan tombol batal
+     * @var boolean
+     */
     private $cancel = true;
+
+    /**
+     * caption untuk tombol ok
+     * @var string
+     */
     private $caption_ok = 'Yakin';
+
+    /**
+     * caption untuk tombol batal
+     * @var [type]
+     */
     private $caption_cancel = 'Batal';
 
+
+    /**
+     * konstruksi kelas
+     * @param string $selector kelas yang menjadi selector
+     */
     public function __construct($selector = '')
     {
-        $this->selector = $selector;
+        $this->selector($selector);
         return $this;
     }
 
+
+    /**
+     * menentukan selector
+     * @param  string $selector kelas yang menjadi selector
+     * @return object
+     */
     public function selector($selector)
     {
         $this->selector = $selector;
         return $this;
     }
 
+
+    /**
+     * mendapatkan selector
+     * @return string selector
+     */
     public function getSelector()
     {
         return $this->selector;
     }
 
+
+    /**
+     * menentukan judul
+     * @param  string $title judul alert
+     * @return object
+     */
     public function title($title)
     {
         $this->title = $title;
         return $this;
     }
 
+
+    /**
+     * menentukan isi alert
+     * @param  string $text isi alert
+     * @return object
+     */
     public function text($text)
     {
         $this->text = $text;
         return $this;
     }
 
+
+    /**
+     * menentukan url tujuan
+     * @param  string $url url yang menjadi tujuan
+     * @return object
+     */
+    public function url($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+
+    /**
+     * hasil
+     * @return string javascript sweet alert
+     */
     public function __toString()
     {
+        $pre_confirm_then = 'then(response => {if(!response.ok){console.log(\'tidak ok\');} return response.json();}).catch(error => {console.log(error);})';
+        $method = '{method:\'DELETE\', headers:{\'X-CSRF-TOKEN\': \''.csrf_token().'\'}}'
+
         $swal = [
             'title' => $this->title,
             'text' => $this->text,
@@ -79,10 +137,18 @@ class MetronicSweetAlert
             'showCancelButton' => $this->cancel,
             'confirmButtonText'=> $this->caption_ok,
             'cancelButtonText' => $this->caption_cancel,
-            'reverseButtons' => true
+            '*preConfirm*' => '*function(){return fetch(\''.$this->url.'\'+value, '.$method.'). '.$pre_confirm_then.'}*'
         ];
 
-        $sc = 'swal.fire('.json_encode($swal).');';
+        $swalencode = json_encode($swal);
+        $swalencode = str_replace('"*', '', $swalencode);
+        $swalencode = str_replace('*"', '', $swalencode);
+
+        $fire_notif = 'console.log(result);Swal.fire(result.title, result.value.text, result.value.icon);';
+        $after_fire = '.then (result => { if(!result.dismiss){ datatable.reload(); '.$fire_notif.'} });';
+
+        $sc = 'var value= $(this).attr("value");';
+        $sc .= 'swal.fire('.$swalencode.')'.$after_fire;
 
         return $sc;
     }
